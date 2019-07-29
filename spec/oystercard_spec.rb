7c308c53727c3f444c9d5d19010be3844a894f25
kierepkae <kierepkaedyta@gmail.com>
2019-07-29 14:28:44 +0100

    finish 05 top up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,17 @@
 require 'oystercard'
 
+# expect{Counter.increment}.to change{Counter.count}.by(2)
+
 describe Oystercard do
-    it { expect(subject.balance).to eq 0 }
+    describe "#balance" do
+      it { expect(subject.balance).to eq 0 }
+    end
+
+    describe '#top_up' do
+
+      it 'can be top up' do
+        expect{subject.top_up 10}.to change{subject.balance}.by(10)
+      end
+
+    end
 end
